f1adef9334197c873dfe125473df3ecedd7de2ac
valentino.orif <valentino.orif@gmail.com>
2025-01-17 12:19:16 +0000

    ajout fichier script.js et début de recup valeur form

---
+++ script.js
@@ -0,0 +1,11 @@
+//récuperation du prénom du bénéficiaire
+let balisePrenom = document.getElementById("prenom")
+let prenom = balisePrenom.value
+
+//récuperation du nom du bénéficiaire
+let baliseNom = document.getElementById("nom")
+let nom = baliseNom.value
+
+//recuperation de l'email du bénéficiaire
+let baliseEmailBenef = document.getElementById("emailbenef")
+let nom = baliseNom.value
