a9cacb7acb206d213be665a3b68f9abc42555fa4
valentino.orif <valentino.orif@gmail.com>
2025-01-17 12:39:07 +0000

    ajout récup pour email msp+rs, date debut+fin, heur debut+fin et motif

--- script.js
+++ script.js
@@ -8,4 +8,32 @@ let nom = baliseNom.value
 
 //recuperation de l'email du bénéficiaire
 let baliseEmailBenef = document.getElementById("emailbenef")
-let nom = baliseNom.value
+let emailBenef = baliseEmailBenef.value
+
+//recuperation de l'email du Msp
+let baliseEmailMsp = document.getElementById("emailmsp")
+let emailMsp = baliseEmailMsp.value
+
+//recuperation de l'email du Rs
+let baliseEmailRs = document.getElementById("emailrs")
+let emailRs = baliseEmailRs.value
+
+//récuperation 
+let baliseDateDebut = document.getElementById("datedebut")
+let dateDebut = baliseDateDebut.value
+
+//récuperation 
+let baliseDateFin = document.getElementById("datefin")
+let dateFin = baliseDateFin.value
+
+//récuperation 
+let baliseHeurDebut = document.getElementById("heurdebut")
+let heurDebut = baliseHeurDebut.value
+
+//récuperation 
+let baliseHeurFin = document.getElementById("heurfin")
+let heurFin = baliseHeurFin.value
+
+//récuperation 
+let baliseMotif = document.getElementById("motif")
+let motif = baliseMotif.value
